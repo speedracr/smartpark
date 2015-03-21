f11daa6b2db7685b933f91a048e9e70031e3392a
speedracr <jonas@robocomobo.com>
2015-03-21 20:36:04 +0100

    Add parkings controller

---
+++ app/controllers/parkings_controller.rb
@@ -0,0 +1,23 @@
+class ParkingsController < ApplicationController
+  def index
+  end
+
+  def new
+    @time = Time.now
+    @parking = "test"
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+
+
+end
